df81cda1917df4ac3eca569c020e313688ca22ec
Mirza Abdur Rehman <126644528+MirzaAbdurRehman@users.noreply.github.com>
2023-12-22 22:09:57 +0500

    VersionControlling

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace cofeeshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the coffee shop and add coffee items to the menu.
+            CoffeeShop coffeeShop = new CoffeeShop();
+            coffeeShop.AddCoffee(new Coffee { Name = "Espresso", Price = 2.5m });
+            coffeeShop.AddCoffee(new Coffee { Name = "Latte", Price = 3.0m });
+            coffeeShop.AddCoffee(new Coffee { Name = "Cappuccino", Price = 3.5m });
+
+            // Main loop for interacting with the coffee shop.
+            while (true)
+            {
+                coffeeShop.DisplayMenu();
+
+                try
+                {
+                    Console.Write("Enter the number of the coffee you'd like to order (0 to exit): ");
+                    int choice = int.Parse(Console.ReadLine());
+
+                    if (choice == 0)
+                    {
+                        break;
+                    }
+
+                    coffeeShop.OrderCoffee(choice);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+        }
+    }
+}
---
+++ coffe.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cofeeshop
+{
+    public class Coffee
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // Represents a coffee shop with a menu and basic operations.
+    public class CoffeeShop
+    {
+        private List<Coffee> menu;
+
+        // Constructor initializes an empty menu.
+        public CoffeeShop()
+        {
+            menu = new List<Coffee>();
+        }
+
+        // Adds a coffee item to the menu.
+        public void AddCoffee(Coffee coffee)
+        {
+            menu.Add(coffee);
+        }
+
+        // Displays the current coffee menu to the console.
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Menu:");
+            for (int i = 0; i < menu.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {menu[i].Name} - ${menu[i].Price:F2}");
+            }
+        }
+
+        // Takes a customer's choice and processes the coffee order.
+        public void OrderCoffee(int choice)
+        {
+            if (IsValidChoice(choice))
+            {
+                Coffee selectedCoffee = menu[choice - 1];
+                Console.WriteLine($"You ordered {selectedCoffee.Name}. Enjoy your coffee!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please select a valid option.");
+            }
+        }
+
+        // Validates if the provided choice is within the valid range.
+        private bool IsValidChoice(int choice)
+        {
+            return choice >= 1 && choice <= menu.Count;
+        }
+    }
+
+}
